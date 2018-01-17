16ea829e8961e0ecb2ad375f80c0d27191482217
Pleasant <zpleasant@anl.gov>
2018-01-17 16:42:08 -0600

    updated map with IL-Gvt compost facilities via geoJSON, looking into customizing markers next

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,31 +1,12 @@
 var map;
 function initMap() {
-    var latlong = {lat: 41.8781, lng: -87.6298};
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: latlong,
-        zoom: 10
-    });
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: {lat: 41.8781, lng: -87.6298}
+  });
 
-    var marker = new google.maps.Marker({
-        map: map,
-        position: latlong,
-        title: 'Hello World!'
-    });
-
-    map.data.loadGeoJson('https://datacatalog.cookcountyil.gov/resource/rj5v-36tj.json');
+  // call ajax to load the geoJSON data
+  map.data.loadGeoJson(
+      'https://datacatalog.cookcountyil.gov/resource/rj5v-36tj.geojson');
 
 }
-
-// use AJAX to call the data from the Cook County API
-$.ajax({
-    url: "https://datacatalog.cookcountyil.gov/resource/rj5v-36tj.json",
-    type: "GET",
-    data: {
-        "$limit" : 5000,
-        "$$app_token" : "CNnPAheIrlHPVLFJFJMxk5baK"
-    }
-
-// console log the data that is retrieved (should be 44 records)
-}).done(function(data) {
-    console.log(data);
-});
