d2b00bee10abfb7087c8929f088e5f3ae96fd456
Pleasant <zpleasant@anl.gov>
2018-01-18 10:59:04 -0600

    changed marker icons and added legend to map

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -2,11 +2,18 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
-    center: {lat: 41.8781, lng: -87.6298}
+    center: {lat: 41.8781, lng: -87.6298},
+    mapTypeId: 'terrain'
   });
 
-  // call ajax to load the geoJSON data
-  map.data.loadGeoJson(
-      'https://datacatalog.cookcountyil.gov/resource/rj5v-36tj.geojson');
+  // call to load the geoJSON data
+  map.data.loadGeoJson('https://datacatalog.cookcountyil.gov/resource/rj5v-36tj.geojson');
+
+  // set specific map styling for geoJSON data
+  map.data.setStyle({
+        icon: 'assets/images/colorIcon_26.png'
+  })
+
+  map.controls[google.maps.ControlPosition.RIGHT_TOP].push (document.getElementById('legend'));
 
 }
